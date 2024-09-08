a45e63720208ab509a70a1298f5d928f379d4c40
kaaarolin <karolin_57@hotmail.com>
2024-09-09 00:11:09 +0200

    ok

---
+++ Labb 1.0/Program.cs
@@ -0,0 +1,6 @@
+﻿// Laboration_1
+
+using System;
+
+Console.WriteLine("Mata in en sträng.");
+string inputString = Console.ReadLine();
---
+++ Labb 1/Program.cs
@@ -0,0 +1,54 @@
+﻿// Laboration 1
+
+using System;
+using System.Data.SqlTypes;
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Mata in din sträng: ");
+string inputString = Console.ReadLine();
+string subString;
+long totalSum = 0; 
+
+for (int i = 0; i < inputString.Length; i++)
+{
+    char firstChar = inputString[i];
+    subString = "";
+
+    if (char.IsLetter(firstChar))
+    {
+        continue;
+    }
+    else if (char.IsDigit(firstChar))
+    {
+        subString += firstChar;
+
+        for (int j = i + 1; j < inputString.Length; j++)
+        {
+            char nextChar = inputString[j];
+
+            if (char.IsLetter(nextChar)) 
+            {
+                break;
+            }
+            subString += nextChar;
+
+            if (firstChar == nextChar)
+            {
+                Console.Write(inputString[0..i]);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(subString);
+                Console.ResetColor();
+                Console.WriteLine(inputString[(j+1)..]);
+                
+                long.TryParse(subString, out long result);
+                totalSum += result;
+
+                break;
+
+            }
+             
+        }
+    } 
+}
+Console.WriteLine("Delsträngarnas totala summa är: " + totalSum);
+
